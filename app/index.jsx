f07eee8fb9e372cb3a537680f9dcd4790f56b351
Jocelyn Faihy <jfaihy@wanadev.fr>
2017-04-10 12:21:26 +0200

    add shortcut to index file

--- app/index.jsx
+++ app/index.jsx
@@ -46,3 +46,48 @@ export default class Form extends Component {
         </form>
     }
 }
+
+export { default as Attributes } from './attributes';
+export { default as ButtonAttributes } from './attributes/button';
+export { default as ContainerAttributes } from './attributes/container';
+export { default as WidgetAttributes } from './attributes/widget';
+
+export { default as Label } from './label';
+export { default as ButtonLabel } from './label/button';
+export { default as ButtonRow } from './row/button';
+export { default as HiddenRow } from './row/hidden';
+export { default as RepeatedRow } from './row/repeated';
+export { default as Rest } from './rest';
+export { default as Row } from './row';
+export { default as Rows } from './rows';
+export { default as Errors } from './errors';
+export { default as Widget } from './widget';
+
+export { default as Checkbox } from './widget/checkbox';
+export { default as Choice } from './widget/choice';
+export { default as Collapsed } from './widget/choice/collapsed';
+export { default as Collection } from './widget/collection';
+export { default as Compound } from './widget/compound';
+export { default as Button } from './widget/button';
+export { default as Date } from './widget/input/date';
+export { default as DateTime } from './widget/input/datetime';
+export { default as DateInterval } from './widget/input/dateinterval';
+export { default as Email } from './widget/input/email';
+export { default as Expanded } from './widget/choice/expanded';
+export { default as File } from './widget/file';
+export { default as Hidden } from './widget/input/hidden';
+export { default as Input } from './widget/input';
+export { default as Integer } from './widget/input/integer';
+export { default as Money } from './widget/input/money';
+export { default as Number } from './widget/input/number';
+export { default as Password } from './widget/input/password';
+export { default as Percent } from './widget/input/percent';
+export { default as Radio } from './widget/radio';
+export { default as Range } from './widget/input/range';
+export { default as Repeated } from '../widget/row/repeated';
+export { default as Reset } from './widget/button/reset';
+export { default as Search } from './widget/input/search';
+export { default as Submit } from './widget/button/submit';
+export { default as Textarea } from './widget/textarea';
+export { default as Time } from './widget/input/time';
+export { default as Url } from './widget/input/url';
